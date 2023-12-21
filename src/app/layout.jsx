e479648bc8084b88547e7bf29d9b169a2131349f
junayed-code <junayed.code@gmail.com>
2023-12-21 11:07:39 +0600

    Initial commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Create Next App",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
