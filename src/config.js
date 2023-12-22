e25db4368c024d568d3086a8d5e036cdb8ba8e99
junayed-code <junayed.code@gmail.com>
2023-12-22 19:26:54 +0600

    Create config.js file

---
+++ src/config.js
@@ -0,0 +1,30 @@
+export default {
+  mainNavItems: [
+    {
+      path: "/",
+      text: "Home",
+    },
+    {
+      path: "/about",
+      text: "About",
+    },
+    {
+      path: "/price",
+      text: "Price",
+    },
+  ],
+  dashboardNavItems: [
+    {
+      path: "/dashboard",
+      text: "Home",
+    },
+    {
+      path: "/dashboard/important",
+      text: "Important",
+    },
+    {
+      path: "/dashboard/completed",
+      text: "Completed",
+    },
+  ],
+};
